e9a4abab4d3552acc69efbb1eb7282cd43d268c0
David <David@10.0.0.159>
2016-07-10 20:29:58 -0500

    Added getMoves() and full col checker to field analyzer

--- src/com/nochindeluxe/fourbot/FieldAnalyzer.java
+++ src/com/nochindeluxe/fourbot/FieldAnalyzer.java
@@ -1,30 +1,59 @@
 package com.nochindeluxe.fourbot;
 
+import java.util.ArrayList;
+
 public class FieldAnalyzer {
     
-    private int[][] field;
+    private Field field;
     private int colNum, rowNum;
     
     public FieldAnalyzer() {
-        
+        field = new Field();
     }
     
     public void updateField(int[][] fieldUpdate) {
-        field = fieldUpdate;
+        colNum = fieldUpdate.length;
+        rowNum = fieldUpdate[0].length;
+        field.update(fieldUpdate);
     }
     
     public int getFirstOpenCell() {
-        int openCellColumn = -1;
-        for(int i=0; i<field.length; i++) {
-            for(int j=0; j<field[0].length; j++) {
-                int cell = field[i][j];
-                if(cell == 0) {
-                    openCellColumn = j;
-                    break;
-                } 
+        int openCellCol = -1;
+        ArrayList<Cell> cells = field.getCells();
+        for(Cell cell : cells) {
+            if(cell.getChecker() == 0) {
+                openCellCol = cell.getCol();
+                break;
             }
         }
-        return openCellColumn;
+        return openCellCol;
     }
+    
+    public ArrayList<Cell> getMoves() {
+        ArrayList<Cell> moves = new ArrayList<>();
+        for(int i=0; i<colNum; i++) {
+            ArrayList<Cell> colCells = field.getCol(i);
+            Cell openCell = getOpenCellForCol(colCells);
+            if(openCell != null) {
+                moves.add(openCell);
+            }
+        }
+        return moves;
+    }
+    
+    public Cell getOpenCellForCol(ArrayList<Cell> colCells) {
+        int openCells = rowNum;
+        for(int i=rowNum-1; i>=0; i--) {
+            Cell cell = colCells.get(i);
+            if(cell.getChecker() > 0) {
+                openCells--;
+            } else {
+                return cell;
+            }
+        }
+        return null;
+    }
+    
+   
     
 }
